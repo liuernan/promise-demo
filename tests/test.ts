047195030a33ad7278089672d521a4a5350a34bb
Jiawei <liujiaweidreamer@gmail.com>
2021-11-10 18:10:25 +0800

    Promise 是一个类

--- tests/test.ts
+++ tests/test.ts
@@ -1,5 +1,4 @@
 import * as chai from 'chai';
-import * as sinon from 'sinon';
 import * as sinonChai from 'sinon-chai';
 import Promise from '../src/promise';
 
@@ -7,14 +6,19 @@ chai.use(sinonChai);
 
 const assert = chai.assert;
 
-console.log(Promise);
+/**
+ * Promises/A+
+ * An open standard for sound, interoperable JavaScript promises—by implementers, for implementers.
+ * https://promisesaplus.com/
+ *
+ * 这是 MDN 官方文档中介绍 Promise 时指向的 Promises/A+ 规范的说明，JS 的 Promise 就是按照这个规范实现的
+ * 所以我们也按照这个规范来写测试用例，一条条都满足，就搞定了
+ * 加油
+ **/
 
-
-describe('test environment', () => {
-  it('init success', () => {
-    const fn = sinon.fake();
-    assert.equal(1, 1);
-    fn();
-    assert.equal(fn.callCount, 1);
+describe('Promise', () => {
+  it('是一个类', () => {
+    assert.isFunction(Promise);
+    assert.isObject(Promise.prototype);
   });
 });
